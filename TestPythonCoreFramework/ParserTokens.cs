7a0485c6dd0a0cae02b2cb1986a490eabf3e6707
Richard Magnor Stenbro <stenbror@hotmail.com>
2025-04-05 23:49:13 +0200

    Added Unittests for False, None and True.

---
+++ TestPythonCoreFramework/ParserTokens.cs
@@ -0,0 +1,36 @@
+﻿using PythonCoreFramework.Parser.Tokens.Keywords;
+
+namespace TestPythonCoreFramework;
+
+public class ParserTokens
+{
+    [Fact]
+    public void ReservedKeywordFalse()
+    {
+        var symbol = new KeywordFalseTokenNode(0, 5, []);
+        Assert.Equal(0u, symbol.Start);
+        Assert.Equal(5u, symbol.End);
+        Assert.Equal([], symbol.Trivia);
+        Assert.Equal("FalseToken( [ 0..5 ] )", symbol.ToString());
+    }
+    
+    [Fact]
+    public void ReservedKeywordNone()
+    {
+        var symbol = new KeywordNoneTokenNode(0, 4, []);
+        Assert.Equal(0u, symbol.Start);
+        Assert.Equal(4u, symbol.End);
+        Assert.Equal([], symbol.Trivia);
+        Assert.Equal("NoneToken( [ 0..4 ] )", symbol.ToString());
+    }
+    
+    [Fact]
+    public void ReservedKeywordTrue()
+    {
+        var symbol = new KeywordTrueTokenNode(0, 4, []);
+        Assert.Equal(0u, symbol.Start);
+        Assert.Equal(4u, symbol.End);
+        Assert.Equal([], symbol.Trivia);
+        Assert.Equal("TrueToken( [ 0..4 ] )", symbol.ToString());
+    }
+}
--- TestPythonCoreFramework/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestPythonCoreFramework;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
